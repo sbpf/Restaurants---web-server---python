37a0ef736f54702ae4d88c04b37e52fa8b73fd9f
Sindhu Shankar <sindhu.pathfinder@gmail.com>
2018-05-18 14:59:28 -0700

    implemented adding new restaurants - Handle POST

--- CRUDwithWebsite.py
+++ CRUDwithWebsite.py
@@ -8,6 +8,13 @@ Base.metadata.bind = engine
 DBSession = sessionmaker(bind=engine)
 session = DBSession()
 
+#Create
+def addRestaurant(restaurantName):
+    newRestaurant = Restaurant(name = restaurantName)
+    session.add(newRestaurant)
+    session.commit()
+
+#Read
 def getAllRestaurants():
     restaurants = session.query(Restaurant).all()
     restaurantsList = []
@@ -15,9 +22,11 @@ def getAllRestaurants():
         restaurantsList.append(restaurant.name)
     return restaurantsList
 
+#Update
 def editRestaurant():
     print "edit"
 
+#Delete
 def deleteRestaurant():
     print "delete"
 
--- webServer-CRUD.py
+++ webServer-CRUD.py
@@ -13,8 +13,9 @@ class webserverHandler(BaseHTTPRequestHandler):
 
                 restaurants = CRUDwithWebsite.getAllRestaurants();
                 output = ""
-                output += "<html><body>Hello!"
-
+                output += "<html><body>Hello!  "
+                output += "<a href = '/restaurants/new'>Add a new Restaurant</a>"
+                
                 for restaurant in restaurants:
                     output += "<p>" + restaurant + "</p>"
                     output += "<a href =" + " " + ">edit</a></br>"
@@ -24,10 +25,50 @@ class webserverHandler(BaseHTTPRequestHandler):
                 self.wfile.write(output)
                 print output
                 return            
-            
+
+            if self.path.endswith("/restaurants/new"):
+                self.send_response(200)
+                self.send_header('Content_type', 'text/html')
+                self.end_headers()
+
+                output = ""
+                output += "<html><body>"            
+                output += """<form method = 'POST' enctype='multipart/form-data' action='/restaurant/new'>
+                           <h2>Add a new restaurant </h2><input name = 'message' type = 'text'><input type = 'submit'></form>"""
+                output += "</body></html>"
+                self.wfile.write(output)
+                print output                
+                
         except IOError:
             self.send_error(404, "File Not Found %s" %self.path)
- 
+
+    def do_POST(self):
+        try:
+            self.send_response(301)
+            self.end_headers()
+            
+            ctype, pdict = cgi.parse_header(self.headers.getheader('content-type'))
+            if ctype == 'multipart/form-data':
+                fields=cgi.parse_multipart(self.rfile, pdict)
+                messagecontent = fields.get('message')
+
+            #Call the function addRestaurant in CRUDwithWebsite, to add the new restaurant to the database
+            CRUDwithWebsite.addRestaurant(messagecontent[0])
+            
+            output = ""
+            output += "<html><body>"
+            output += "<h1> %s successfully added </h1>" % messagecontent[0]
+            output += "<a href = '/restaurants'>Back to list of Restaurants</a>"
+
+            output += """<form method = 'POST' enctype='multipart/form-data' action='/restaurant/new'>
+                       <h2>Add a new restaurant </h2><input name = 'message' type = 'text'><input type = 'submit'></form>"""
+            output += "</body></html>"
+            self.wfile.write(output)
+            print output
+
+        except:
+            self.send_error(404, "File Not Found %s" %self.path)
+                
 #entry point of code
 def main():
     try:
